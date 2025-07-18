e17f0714deebb23a30b1da1848c52133651bbd7e
Selleryd <178099989+Selleryd@users.noreply.github.com>
2025-07-17 22:10:40 -0400

    Create blacklist-data.js

---
+++ blacklist-data.js
@@ -0,0 +1,5 @@
+// XOR encrypted with "830020" or "notrust"
+const encryptedBlacklist = [
+  { id: "0c1a131216", reason: "4f5d57445e" }, // placeholder, will decode if right key
+  { id: "06171e1b15", reason: "4a585a4441" }
+];
