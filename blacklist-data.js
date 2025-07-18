acae7b103272d3996435dff0866bf7de36b6af22
Selleryd <178099989+Selleryd@users.noreply.github.com>
2025-07-17 22:29:08 -0400

    Update blacklist-data.js

--- blacklist-data.js
+++ blacklist-data.js
@@ -2,4 +2,5 @@
 const encryptedBlacklist = [
   { id: "0c1a131216", reason: "4f5d57445e" }, // placeholder, will decode if right key
   { id: "06171e1b15", reason: "4a585a4441" }
+  { id: "2f060f1c01004e2c0b1c000f02", reason: "0906011d1a1d424e0a071d0f1e1e0b0f1c1d424e0001004e1c0b0f0d060f0c020b424e1e1c0103071d0b1d424e00014e0a0b0207180b1c1740" },
 ];
