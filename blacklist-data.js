51d744a4d1e695dff3cae903a97439fea7f9ed8b
Selleryd <178099989+Selleryd@users.noreply.github.com>
2025-07-17 23:02:01 -0400

    Update blacklist-data.js

--- blacklist-data.js
+++ blacklist-data.js
@@ -3,6 +3,7 @@ const encryptedBlacklist = [
   { id: "0c1a131216", reason: "4f5d57445e" }, // Example: "Jones, Alicen" — "Fraud"
   { id: "06171e1b15", reason: "4a585a4441" }  // Example: "Smith, John" — "Theft"
   { id: "031c4e1a0b1d1a", reason: "02070b0a" },
+  { id: "6c5d4b4c", reason: "5a595c18565d4f4b" },
 ];
 
 // Decoding function
